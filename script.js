85c62480cd2f68de89e9e056a2e1a7c8038f2e99
Umar Bello Tafida <154561069+umarbellotafida@users.noreply.github.com>
2025-07-08 01:28:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -51,3 +51,14 @@ function updateClock() {
 
 setInterval(updateClock, 1000);
 updateClock();
+
+// Clock & date (Nigeria)
+function updateClock() {
+  const now = new Date();
+  const optionsTime = { timeZone: 'Africa/Lagos', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+  const optionsDate = { timeZone: 'Africa/Lagos', weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
+  document.getElementById('clock').textContent = now.toLocaleTimeString('en-NG', optionsTime);
+  document.getElementById('date').textContent = now.toLocaleDateString('en-NG', optionsDate);
+}
+setInterval(updateClock, 1000);
+updateClock();
